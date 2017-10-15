d7396cf66695e4207790284195e337de76054dae
Hugo Meza <hugo_meza94@hotmail.com>
2017-10-15 05:46:13 -0400

    Include Accessibility of basic db

--- app/src/main/java/com/example/kevin/ecparking/MapsActivity.java
+++ app/src/main/java/com/example/kevin/ecparking/MapsActivity.java
@@ -43,4 +43,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+    public void gatherDataBase(){
+        StreetsSize Size = new StreetsSize();
+        Size.openFile();
+        Size.setSize();
+        Size.closeFile();
+
+
+        int numOfStreets = Size.getSize();
+
+        Street nextStreetInfo = new Street();
+        nextStreetInfo.openFile();
+        Street street[] = new Street[numOfStreets];
+
+        for(int i = 0; i < numOfStreets; i++) {
+            street[i] = new Street();
+            street[i].inLat = nextStreetInfo.getElement();
+            street[i].inLon = nextStreetInfo.getElement();
+            street[i].endLat = nextStreetInfo.getElement();
+            street[i].endLon = nextStreetInfo.getElement();
+            street[i].ava = nextStreetInfo.getElement();
+        }
+
+        nextStreetInfo.closeFile();
+    }
 }
